b06723e4e8485b1d80c56678bb32f071d47ab929
nausm <nausm9714@oslomet.no>
2025-01-28 18:56:20 +0100

    oppg 1.1

--- src/no/hvl/data102/filmarkiv/impl/Film.java
+++ src/no/hvl/data102/filmarkiv/impl/Film.java
@@ -1,5 +1,38 @@
 package no.hvl.data102.filmarkiv.impl;
 
 public class Film {
+	public Film(int filmNr, String tittel, int lansering, String filmselskap) {
+		this.filmNr = filmNr;
+		this.tittel = tittel;
+		this.lansering = lansering;
+		this.filmselskap = filmselskap;
+	}
+	
+	
+	private int filmNr;
+	private String tittel;
+	private int lansering;
+	private String filmselskap;
+		
+	private enum Sjanger{
+		ACTION, DRAMA, HISTORY, SCIFI;
+	}
 
+	
+	public int getFilmNr(){
+	return filmNr;
+	}
+	
+	public  String getTittel() {
+		return tittel;
+		
+	}
+	
+	public  int getLansering() {
+		return lansering;			
+	}
+	
+	public  String getFilmselskap() {
+		return filmselskap;			
+	}
 }
