55eeef13832027c86cf24625b7626655947e5972
nausm <nausm9714@oslomet.no>
2025-01-28 13:45:50 +0100

    Lagde filer

---
+++ src/no/hvl/data102/filmarkiv/test/FilmarkivTest.java
@@ -0,0 +1,14 @@
+package no.hvl.data102.filmarkiv.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FilmarkivTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
